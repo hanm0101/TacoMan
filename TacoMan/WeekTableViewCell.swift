7e6fba0958bf93ceaa55eb19b78f24f22289bf5e
hana <hana.ballet0101@gmail.com>
2016-02-10 20:47:40 +0900

    first page set up two IBoutlet's

---
+++ TacoMan/WeekTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  WeekTableViewCell.swift
+//  TacoMan
+//
+//  Created by hana on 10/2/2016.
+//  Copyright © 2016 hana. All rights reserved.
+//
+
+import UIKit
+
+class WeekTableViewCell: UITableViewCell {
+
+    @IBOutlet var Weekday: UILabel!
+    @IBOutlet var Date: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
